ef1411517e6d38b11c80c46d741b7ec0a1d2012b
Leonidas Gonzalez <lgonzalez@cxc.com.mx>
2023-05-10 14:44:42 -0600

    Implemented kill data

--- DTOs/KillData.cs
+++ DTOs/KillData.cs
@@ -8,11 +8,25 @@ using System.Threading.Tasks;
 namespace HyperManager.DTOs {
 	public struct KillData : IParseable {
 
+		private const string FORCE_MARKER = "--force";
+
 		public string target;
 		public bool force;
 
 		public IParseable SetFromCommand(CommandBuilder command) {
-			throw new NotImplementedException();
+			int argc = command.ArgsCount;
+			if (argc < 1) {
+				throw new Exception("Not enough arguments provided for Kill command! Run with: Help Kill");
+			}
+			for (int i = 0; i < argc; i++) {
+				string normalizedArg = command.GetArgument(i).ToLower();
+				if (normalizedArg.StartsWith(FORCE_MARKER)) {
+					this.force = true;
+					continue;
+				}
+				this.target = normalizedArg;
+			}
+			return this;
 		}
 	}
 }
